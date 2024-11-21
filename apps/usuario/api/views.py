819ef39d9d6e99c12fe5c604429a25929ae40e13
wilson_rodriguez09 <samboniwilson09@gmail.com>
2024-11-20 20:39:42 -0500

    correcion de over ride

--- apps/usuario/api/views.py
+++ apps/usuario/api/views.py
@@ -1,11 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 from apps.usuario.models import Rol, Usuario
 from apps.usuario.serializer import RolSerializer, UsuarioSerializer
-
+from apps.usuario.api.permissions import IsUsuarioReadOnly
 class RolViewSet(ModelViewSet):
     queryset = Rol.objects.all()
     serializer_class = RolSerializer
 
 class UsuarioViewSet(ModelViewSet):
+    permission_classes = [IsUsuarioReadOnly]
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
