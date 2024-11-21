21fdaec474e1a8e6b47902f07265f4632f7a586b
wilson_rodriguez09 <samboniwilson09@gmail.com>
2024-11-20 21:21:50 -0500

    desarrollo de overide con documentacion y permisos final

--- apps/usuario/api/permissions.py
+++ apps/usuario/api/permissions.py
@@ -2,7 +2,7 @@ from rest_framework.permissions import BasePermission
 
 class IsUsuarioReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if request.method == "GET":
+        if request.method == "GET, POST":
             return True
         else:
             return request.user.is_staff
