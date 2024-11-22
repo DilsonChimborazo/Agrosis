fc9ae545593aff1ebf8b49c8d5fc642e07912586
DilsonChimborazo <dilsonchimborazoperez@gmail.com>
2024-11-21 20:53:55 -0500

    Eliminar archivos .pyc y resolver conflictos

--- Agrosis/urls.py
+++ Agrosis/urls.py
@@ -19,13 +19,10 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('api/sensor',include(router_Sensores.urls)),
-    path('api/mide',include(router_Mide.urls)),
+   path('admin/', admin.site.urls),
+   path('api/', include(router.urls)),
+   path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   path('api/sensor',include(router_Sensores.urls)),
+   path('api/mide',include(router_Mide.urls)),
 ]
-
-
-    
