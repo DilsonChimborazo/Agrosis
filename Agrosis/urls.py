894e4efd0eddc93d18c5524b9a5c6c0d903784a6
DilsonChimborazo <dilsonchimborazoperez@gmail.com>
2024-11-28 17:48:32 -0500

    solucion de rutas en el archivo urls principal

--- Agrosis/urls.py
+++ Agrosis/urls.py
@@ -20,6 +20,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 #routers de la rama de iot 
+from apps.iot.mide.api.router import router
 
 schema_view = get_schema_view(
    openapi.Info(
