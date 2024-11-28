ee2d0e40be372678b80fe68f3cb691eeb95c3c41
wilson_rodriguez09 <samboniwilson09@gmail.com>
2024-11-27 21:12:15 -0500

    Correccion de routers y migraciones a la base de datos, modulo usuarios

--- apps/usuarios/rol/api/router.py
+++ apps/usuarios/rol/api/router.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
 from apps.usuarios.rol.api.views import RolViewSet
 
-router = DefaultRouter
+router = DefaultRouter()
 router.register(prefix='rol', viewset=RolViewSet)
