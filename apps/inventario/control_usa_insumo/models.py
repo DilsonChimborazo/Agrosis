4457df50436d73ce2c9af3ac7bb8114912ed99f2
DilsonChimborazo <dilsonchimborazoperez@gmail.com>
2024-11-30 16:50:15 -0500

    Commit para verificacion de actualizacion

--- apps/inventario/control_usa_insumo/models.py
+++ apps/inventario/control_usa_insumo/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from apps.inventario.insumo.models import Insumo
+from apps.trazabilidad.Control_Fitosanitario.models import ControlFitosanitario
 
 # Create your models here.
 class ControlUsaInsumo(models.Model):
     fk_id_insumo = models.ForeignKey(Insumo, on_delete=models.SET_NULL, null=True)
-    fk_id_control_fitosanitario = models.ForeignKey('ControlFitosanitario', on_delete=models.SET_NULL, null=True)
+    fk_id_control_fitosanitario = models.ForeignKey(ControlFitosanitario , on_delete=models.SET_NULL, null=True)
     cantidad = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return f"Control: {self.id_control_usa_insumo} - Insumo: {self.fk_id_insumo.nombre}"
+        return f"Control: {self.cantidad} - Insumo: {self.fk_id_insumo.nombre}"
