cccc0e390a6f1902ee58619f833b1588232c63b8
lucy2003fer <lucy48fernada@gmail.com>
2024-12-04 13:24:45 -0500

    agregué en las views de cada app la autenticacion JWT

--- apps/finanzas/produccion/api/views.py
+++ apps/finanzas/produccion/api/views.py
@@ -1,7 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from apps.finanzas.produccion.models import Produccion
+from rest_framework.permissions import IsAutenticated
 from apps.finanzas.produccion.api.serializer import ProduccionSerializer
 
 class ProduccionViewSet(ModelViewSet):
+    permissions_clases = [IsAutenticated]
+    
     queryset = Produccion.objects.all()
     serializer_class = ProduccionSerializer
