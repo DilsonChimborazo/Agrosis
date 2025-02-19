6de6a0282df047b00851c5ce8e0fbe817a1ffb0f
DilsonChimborazo <dilsonchimborazoperez@gmail.com>
2025-02-19 16:32:32 -0500

    websocked tabla de eras

--- apps/iot/eras/api/router.py
+++ apps/iot/eras/api/router.py
@@ -1,6 +1,13 @@
 from rest_framework.routers import DefaultRouter
 from apps.iot.eras.api.views import ErasViewSet
+from django.urls import re_path
+from apps.iot.eras.api.consumer import ErasConsumer
 
 router_Eras = DefaultRouter()
 router_Eras.register(prefix="eras", basename="eras", viewset=ErasViewSet)
 
+
+#ruta del websocked para eras
+websocket_urlpatterns = [
+    re_path(r'ws/eras/$', ErasConsumer.as_asgi()),
+]
