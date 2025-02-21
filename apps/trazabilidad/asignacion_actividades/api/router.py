75e668c32384610c4523dfc8cd6159ea5a1d3ebd
Pacho17 <fransuarez172005@gmail.com>
2025-02-20 19:03:47 -0500

    websocket en asignacion_actividades en trazabilidad

--- apps/trazabilidad/asignacion_actividades/api/router.py
+++ apps/trazabilidad/asignacion_actividades/api/router.py
@@ -1,8 +1,12 @@
 from rest_framework.routers import DefaultRouter
 from apps.trazabilidad.asignacion_actividades.api.views import Asignacion_actividadesModelViewSet
+from django.urls import re_path
+from apps.trazabilidad.asignacion_actividades.api.consumer import Asignacion_actividadesConsumer
 
 # Crear routers específicos para cada grupo de ViewSets
 routerAsignacion_actividades = DefaultRouter()
 routerAsignacion_actividades.register(prefix='asignaciones_actividades', viewset=Asignacion_actividadesModelViewSet, basename='asignaciones_actividades')
 
-
+websocket_urlpatterns = [
+    re_path(r'ws/asignacion_actividades/$', Asignacion_actividadesConsumer.as_asgi()),
+]
--- apps/trazabilidad/asignacion_actividades/models.py
+++ apps/trazabilidad/asignacion_actividades/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from apps.trazabilidad.actividad.models import Actividad
 from apps.usuarios.usuario.models import Usuarios
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from channels.layers import get_channel_layer
+import json
 
 # Create your models here.
 class Asignacion_actividades(models.Model):
@@ -11,3 +15,25 @@ class Asignacion_actividades(models.Model):
     
     def __str__(self): 
         return f'{self.fk_id_actividad} - {self.id_identificacion}'
+
+
+@receiver(post_save, sender=Asignacion_actividades)
+def enviar_datos_asignacion_actividades(sender, instance, **kwargs):
+    channel_layer = get_channel_layer()
+    data = {
+        "fecha": instance.fecha,
+        "fk_id_actividad": instance.fk_id_actividad,
+        "id_identificacion": instance.id_identificacion
+        
+    }
+    async def send_data():
+        await channel_layer.group_send(
+            "asignacion_actividades",
+            {
+                "type": "asignacion_actividades_data",
+                "message": data
+            }
+        )
+    
+    import asyncio
+    asyncio.run(send_data())
